eda0479f74cdce9a137baa92d54c938529f76e0b
Nicolas Lemblé <nicolas.lemble@gmail.com>
2024-10-30 22:02:01 +0100

    add lualine as status line

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -179,4 +179,11 @@ return {
       },
     },
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    init = function()
+      require('lualine').setup()
+    end,
+  },
 }
